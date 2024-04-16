5266c91ee9d3f0cfc510018cc375daf260f5be31
Андрей <andrej.chugunov2002@yandex.ru>
2024-04-16 18:30:41 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -37,12 +37,48 @@ public class Main {
         }
         System.out.println("Задание 3");
         {
-            short year = 2023;
+            short year = 2019;
             boolean firstYear = (year < 1584);
             if (firstYear) {
                 System.out.println(" Введите значение больше 1584");
+            } else if (year % 4 == 0 || year % 400 == 0) {
+                System.out.println(" Год является високосным");
+            } else if (year % 100 != 0) {
+                System.out.println(" Год не является високосным");
             }
-
+        }
+        System.out.println("Задание 4");
+        {
+            short deliveryDistance = 95;
+            if (deliveryDistance <= 20) {
+                System.out.println(" Потребуется дней: 1");
+            } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+                System.out.println(" Потребуется дней: 2");
+            } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+                System.out.println(" Потребуется дней: 3");
+            } else if (deliveryDistance > 100) {
+                System.out.println(" Свыше 100 км доставки нет");
+            }
+        }
+        System.out.println("Задание 5");
+        {
+            byte monthNumber = 11;
+            switch (monthNumber) {
+                case 12, 1, 2:
+                    System.out.println(" Зима");
+                    break;
+                case 3, 4, 5:
+                    System.out.println(" Весна");
+                    break;
+                case 6, 7, 8:
+                    System.out.println(" Лето");
+                    break;
+                case 9, 10, 11:
+                    System.out.println(" Осень");
+                    break;
+                default:
+                    System.out.println(" Такого сезона не существует");
             }
         }
     }
+}
